e4211d5339f56512c52576d46881ac83919f8a1d
David Ellinger <david.ellinger@me.com>
2021-10-07 19:26:20 -0400

    final fixes

--- app/cli.py
+++ app/cli.py
@@ -1,11 +1,14 @@
 # cli.py
 import click
 from dotenv import load_dotenv
+
 load_dotenv()  # take env variables from .env
+
 
 @click.command()
 def main():
     print("I'm a beautiful CLI ✨")
+
 
 @click.group()
 def cli():
@@ -16,9 +19,11 @@ def cli():
 def budgets():
     click.echo("Returned budgets")
 
+
 @cli.command()
 def transactions():
     click.echo("Returned transactions")
+
 
 if __name__ == "__main__":
     main()
--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,7 @@ err=$?
 if [ "$err" -ne 5 ] && [ "$err" -ne 0 ]; then
   exit "$err"
 fi
-flake8
+isort .
+black .
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
